89f6e567652dc70913f701b99c9cc167f3d2825a
Kennybabs-py <babalolakehinde74@gmail.com>
2022-06-29 08:47:14 +0100

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.scss";
+import logo from "../../assets/images/hero/mc_logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+        <div>
+          <span>Magic</span>
+          <span>Card</span>
+        </div>
+      </div>
+      <ul className="site_links">
+        <li>About</li>
+        <li>Roadmap</li>
+        <li>Community</li>
+      </ul>
+
+      <ul className="sm_links">
+        <li>
+          <a href="link">tweet</a>
+        </li>
+        <li>
+          <a href="link">discord</a>
+        </li>
+        <li>
+          <a href="link">telegram</a>
+        </li>
+      </ul>
+
+      <button>Connect wallet</button>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Landing.scss";
+import heroImg from "../../assets/images/hero/hero_image.png";
+
+const Landing = () => {
+  return (
+    <main>
+      <div className="hero">
+        <div className="hero_texts">
+          <div className="first">
+            <span>MINT</span>
+            <br />
+            <span>NFT</span>
+            <br />
+            <span>GIFT CARDS</span>
+          </div>
+          <div className="second">
+            <span>SEND AND CLAIM</span>
+            <br />
+            <span>GIFT CARDS AND TOKENS</span>
+          </div>
+          <div className="third">
+            <span>From family, friends, co-workers</span>
+            <br />
+            <span>&#38; acquaintances</span>
+          </div>
+        </div>
+
+        <div className="hero_image">
+          <img src={heroImg} alt="heroimg" />
+          <div className="img_shadow"></div>
+        </div>
+      </div>
+
+      <div className="mint-claim_btn">
+        <button>MINT</button>
+        <button>CLAIM</button>
+      </div>
+
+      <div className="newsletter">
+        <div>texts texts</div>
+        <form action="">
+          <input type="text" />
+          <br />
+          <button>Subscribe to Newletter</button>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default Landing;
