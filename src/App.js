398ceda828b3f6b04fcc8d346f7b76074046210d
EllaLawrie <ellalawrie002@gmail.com>
2022-07-02 00:13:30 +0100

    Landing page removed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import "./styles/App.scss";
-import Header from "./components/Header/Header";
-// import Landing from "./pages/Landing/Landing";
-import EditCard from "./pages/EditCard/EditCard";
+import './styles/App.scss'
+import Header from './components/Header/Header'
+import EditCard from './pages/EditCard/EditCard'
 // import Mint from "./pages/Mint/Mint";
-import Footer from "./components/Footer/Footer";
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
-      {/* <Landing /> */}
       <EditCard />
       {/* <Mint /> */}
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
