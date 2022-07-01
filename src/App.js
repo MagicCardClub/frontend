ce689bf786820a11cf4fcc2da262dc86c5fb76a7
EllaLawrie <ellalawrie002@gmail.com>
2022-07-02 00:38:06 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-import "./styles/App.scss";
-import Header from "./components/Header/Header";
-// import Landing from "./pages/Landing/Landing";
-import EditCard from "./pages/EditCard/EditCard";
-// import Mint from "./pages/Mint/Mint";
-import Footer from "./components/Footer/Footer";
-=======
 import './styles/App.scss'
 import Header from './components/Header/Header'
 import Landing from './pages/Landing/Landing'
 import Footer from './components/Footer/Footer'
->>>>>>> 45055b57e2ba6bcb9236bd44f498e240c3f76bc8
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      {/* <Landing /> */}
-      <EditCard />
-      {/* <Mint /> */}
+      <Landing />
       <Footer />
     </div>
   )
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,68 +1,67 @@
-import React from "react";
-import "./Landing.scss";
-import heroImg from "../../assets/images/hero/hero_image.png";
-import heroImageMobile from "../../assets/images/hero/heroImageMobile.png";
+import React from 'react'
+import './Landing.scss'
+import heroImg from '../../assets/images/hero/hero_image.png'
+import heroImageMobile from '../../assets/images/hero/heroImageMobile.png'
 
 const Landing = () => {
   return (
     <main>
       {/* different patches of colors on page */}
-<<<<<<<< HEAD:src/pages/Landing/Landing.js
       {/* <div className="color_one"></div>
       <div className="color_two"></div>
       <div className="color_three"></div> */}
-========
-      <div className="color_one"></div>
-      <div className="color_two"></div>
-      <div className="color_three"></div>
->>>>>>>> 45055b57e2ba6bcb9236bd44f498e240c3f76bc8:src/components/Landing/Landing.js
-      <div className="hero">
-        <div className="hero_texts">
-          <div className="first">
+
+      <div className='color_one'></div>
+      <div className='color_two'></div>
+      <div className='color_three'></div>
+
+      <div className='hero'>
+        <div className='hero_texts'>
+          <div className='first'>
             <span>MINT</span>
             <br />
             <span>NFT</span>
             <br />
             <span>GIFT CARDS</span>
           </div>
-          <div className="second">
+          <div className='second'>
             <span>SEND AND CLAIM</span>
             <br />
             <span>GIFT CARDS AND TOKENS</span>
           </div>
-          <div className="third">
+          <div className='third'>
             <span>From family, friends, co-workers</span>
             <br />
             <span>&#38; acquaintances</span>
           </div>
         </div>
 
-        <div className="hero_image">
-          <img src={heroImg} alt="heroimg" />
-          <img src={heroImageMobile} alt="heroimg" />
+        <div className='hero_image'>
+          <img src={heroImg} alt='heroimg' />
+          <img src={heroImageMobile} alt='heroimg' />
 
-          <div className="img_shadow"></div>
+          <div className='img_shadow'></div>
         </div>
       </div>
 
-      <div className="mint-claim_btn">
+      <div className='mint-claim_btn'>
         <button>MINT</button>
         <button>CLAIM</button>
       </div>
 
-      <div className="newsletter">
-        <div className="text">
+      <div className='newsletter'>
+        <div className='text'>
           Get the latest update on upcoming gift card <br /> types for your
           family, friends and loved ones.
         </div>
-        <form action="">
-          <input type="text" placeholder="enter email" />
+        <form action=''>
+          <input type='text' placeholder='enter email' />
           <br />
           <button>Subscribe to Newletter</button>
         </form>
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default Landing;
+export default Landing
